c0f3d65a029135940731d9f99e7bf5cd682e93d6
Jonathan <jonathan.max.annett@gmail.com>
2021-05-22 12:48:02 +1000

    Update glitch-startup.js

--- glitch-startup.js
+++ glitch-startup.js
@@ -1,70 +1,9 @@
-function serverStopper(server) {
-  // incoporates ideas from https://dev.to/gajus/how-to-terminate-a-http-server-in-node-js-ofk
-  const sockets = new Set();
-
-  server.on("connection", function(socket) {
-    sockets.add(socket);
-
-    server.once("close", () => {
-      sockets.delete(socket);
-    });
-  });
-
-  stop.sockets = sockets;
-
-  return stop;
-
-  /**
-   * Forcefully terminates HTTP server.
-   */
-  function stop(cb) {
-    for (const socket of sockets) {
-      socket.destroy();
-      sockets.delete(socket);
-    }
-    server.close(function() {
-      if (typeof cb === "function") {
-        cb();
-      }
-    });
-  }
-}
-
 module.exports = function(app,express) {
   const listener = app.listen(process.env.PORT, function() {
     console.log("Your app is listening on port " + listener.address().port);
   });
-  const stopper = serverStopper(listener);
   const self = {};
   const implementation = {
-    listeners: {
-      value: {
-        http: listener,
-        https: listener
-      },
-      enumerable: true,
-      writable: false
-    },
-    stop: {
-      value: function(cb) {
-        stopper(function() {
-          if (typeof cb === "function") {
-            cb();
-          }
-        });
-      },
-      enumerable: true,
-      writable: false
-    },
-    connections: {
-      get: function() {
-        return Array.prototype.slice.call(stopper.sockets, 0);
-      },
-      set: function() {
-        /*ignored*/
-      },
-      enumerable: true
-    }
   };
   Object.defineProperties(self, implementation);
   return self;
