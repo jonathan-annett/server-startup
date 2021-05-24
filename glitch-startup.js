bf2656d9a813d0111576c370b56e7167f12d3ec2
Jonathan <jonathan.max.annett@gmail.com>
2021-05-24 13:39:55 +1000

    Update glitch-startup.js

--- glitch-startup.js
+++ glitch-startup.js
@@ -2,9 +2,13 @@ module.exports = function(app,express) {
   const listener = app.listen(process.env.PORT, function() {
     console.log("Your app is listening on port " + listener.address().port);
   });
+  
+  if (typeof app.__on_server==='function') {
+        app.__on_server(undefined,listener);
+  }
+  
   const self = {};
-  const implementation = {
-  };
+  const implementation = { };
   Object.defineProperties(self, implementation);
   return self;
 };
